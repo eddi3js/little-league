4a7b572be12dc89ca23e63befee14be8951ebc35
Eddie Sanmarco <eddie@Eddies-MacBook-Pro.local>
2022-10-16 13:01:35 -0700

    changed title of app

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import Navbar from "./navbar";
 import Sidebar from "./sidebar";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Head>
+        <title>Leagues.run</title>
+      </Head>
       <Navbar />
       <div className="flex flex-1 flex-col-reverse items-stretch md:flex-row">
         <Sidebar />
